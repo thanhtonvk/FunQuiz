d7fd88ae6f5e896f1fc3b2163d99e646689058f9
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2021-12-22 16:04:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/funquiz/Database/BaoCaoDBContext.java
@@ -0,0 +1,31 @@
+package com.example.funquiz.Database;
+
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+
+import com.example.funquiz.Model.BaoCao;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class BaoCaoDBContext {
+    public FirebaseDatabase mDatabase;
+    public DatabaseReference mRef;
+    public ArrayList<BaoCao> baoCaoArrayList;
+
+    public BaoCaoDBContext() {
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference("baocao");
+        baoCaoArrayList = new ArrayList<>();
+    }
+
+    public void addBaoCao(BaoCao baoCao) {
+        mRef.child(baoCao.getId()).setValue(baoCao);
+    }
+
+}
